df20a7d8e13e6721e02fa44aabe41c38216b348a
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-02-17 12:49:04 -0200

    more than 1 word

--- app/models/generator.rb
+++ app/models/generator.rb
@@ -3,7 +3,7 @@ class Generator
   WORDS = ["Full Stack", "Front-end", "Back-end", "UX", "Developer", "Designer", "Researcher", "Trainee", "Customer", "Engineer", "Software", "Product", "Owner", "Manager", "Agent"]
 
   def generate
-    WORDS.sample(rand(1..4)).join ' '
+    WORDS.sample(rand(2..4)).join ' '
   end
 
 end
